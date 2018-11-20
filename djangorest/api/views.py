439a4ebd9c230820fc2f1123f9fb0dca5df00d64
Tawakalt <olaniyitaiwo95@yahoo.com>
2018-11-20 19:15:14 +0100

    implement authentication and authorization

--- djangorest/api/views.py
+++ djangorest/api/views.py
@@ -1,22 +1,28 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
+from .permissions import IsOwner
 from .serializers import BucketlistSerializer
 from .models import Bucketlist
 
 # Create your views here.
 class CreateView(generics.ListCreateAPIView):
-    """This class defines the create behavior of our rest api."""
+    """This class handles the GET and POSt requests of our rest api."""
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
 
     def perform_create(self, serializer):
         """Save the post data when creating a new bucketlist."""
-        serializer.save()
+        serializer.save(owner=self.request.user)
 
+    def get_queryset(self):
+        """Only return bucketlist items owned by the currently authenticated user."""
+        user = self.request.user
+        return Bucketlist.objects.filter(owner=user)
 
 
 class DetailsView(generics.RetrieveUpdateDestroyAPIView):
-    """This class handles the http GET, PUT and DELETE requests."""
+    """This class handles GET, PUT, PATCH and DELETE requests."""
 
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
